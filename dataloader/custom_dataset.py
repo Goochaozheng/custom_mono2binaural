1a38da64af6fabb2615acf523daa1c024e403b71
CharlesGuo <gchaozheng@gmail.com>
2020-04-10 01:11:57 +0800

    with resnet

--- dataloader/custom_dataset.py
+++ dataloader/custom_dataset.py
@@ -25,6 +25,7 @@ class CustomDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
 
         frame = transforms.ToTensor()(self.data_source['frame'][idx])
+        frame = torch.FloatTensor(frame)
         audio_mix = torch.FloatTensor(self.data_source['audio_mix'][idx])
         aduio_diff = torch.FloatTensor(self.data_source['audio_diff'][idx])
 
