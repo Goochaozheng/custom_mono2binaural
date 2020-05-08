0fc61c9f36b509cb94320968bbb84d9a10a117f1
CharlesGuo <gchaozheng@gmail.com>
2020-05-08 14:36:44 +0800

    xavier init

--- models/networks.py
+++ models/networks.py
@@ -52,15 +52,12 @@ def weights_init(m):
     #initialize weights normal_(mean, std)
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm2d') != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.fill_(0)
-    elif classname.find('BatchNorm3d') != -1:
-        m.weight.data.normal_(1.0, 0.02)
+        torch.nn.init.kaiming_normal_(m.weight)
+    elif classname.find('BatchNorm') != -1:
+        torch.nn.init.kaiming_normal_(m.weight)
         m.bias.data.fill_(0)
     elif classname.find('Linear') != -1:
-        m.weight.data.normal_(0.0, 0.02)
+        torch.nn.init.kaiming_normal_(m.weight)
 
 # 3d conv
 class VisualNet(nn.Module):
