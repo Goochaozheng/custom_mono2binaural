bcc72569500f03f3f43ff04be089f40e8a406463
CharlesGuo <gchaozheng@gmail.com>
2020-05-09 20:25:19 +0800

    modify visual net

--- models/networks.py
+++ models/networks.py
@@ -62,29 +62,34 @@ def weights_init(m):
 class VisualNetCropped(nn.Module):
     def __init__(self):
         super(VisualNetCropped, self).__init__()
-        original_resnet = torchvision.models.resnet18(pretrained=True)
-        layers = list(original_resnet.children())[0:-2]
-        self.feature_extraction = nn.Sequential(*layers) #features before conv1x1
-        self.feature_compression = create_conv(input_channels=512, output_channels=64, kernel=1, paddings=0, stride=1)
+        self.visual_conv1 = create_conv(3, 16, kernel=8, paddings=2, stride=4)
+        self.visual_conv2 = create_conv(16, 32)
+        self.visual_conv3 = create_conv(32, 64)
+        self.visual_conv4 = create_conv(64, 64)
 
     def forward(self, frame):
-        visual_feature = self.feature_extraction(frame)
-        visual_feature = self.feature_compression(visual_feature)
-        return visual_feature #(, 64, 4, 4)
+        visual_feature = self.visual_conv1(frame)
+        visual_feature = self.visual_conv2(visual_feature)
+        visual_feature = self.visual_conv3(visual_feature)
+        visual_feature = self.visual_conv4(visual_feature)        
+        return visual_feature #(, 64, 2, 2)
 
 
 class VisualNetGlobal(nn.Module):
     def __init__(self):
         super(VisualNetGlobal, self).__init__()
-        original_resnet = torchvision.models.resnet18(pretrained=True)
-        layers = list(original_resnet.children())[0:-2]
-        self.feature_extraction = nn.Sequential(*layers) #features before conv1x1
-        self.feature_compression = create_conv(input_channels=512, output_channels=16, kernel=1, paddings=0, stride=1)
+        self.visual_conv1 = create_conv(3, 16, kernel=8, paddings=2, stride=4)
+        self.visual_conv2 = create_conv(16, 32, kernel=8, paddings=2, stride=4)
+        self.visual_conv3 = create_conv(32, 64)
+        self.visual_conv4 = create_conv(64, 64)
 
     def forward(self, frame):
-        visual_feature = self.feature_extraction(frame)
-        visual_feature = self.feature_compression(visual_feature)    
-        return visual_feature #(, 32, 4, 8)
+
+        visual_feature = self.visual_conv1(frame)
+        visual_feature = self.visual_conv2(visual_feature)
+        visual_feature = self.visual_conv3(visual_feature)
+        visual_feature = self.visual_conv4(visual_feature)        
+        return visual_feature #(, 64, 2, 4)
 
 
 class AudioNet(nn.Module):
@@ -98,10 +103,10 @@ class AudioNet(nn.Module):
         self.audionet_convlayer5 = create_conv(ngf * 8, ngf * 8)
 
         self.audionet_upconvlayer1 = create_upconv(2048, ngf * 8)
-        self.audionet_upconvlayer2 = create_upconv(ngf * 16, ngf *4)
-        self.audionet_upconvlayer3 = create_upconv(ngf * 8, ngf * 2)
-        self.audionet_upconvlayer4 = create_upconv(ngf * 4, ngf)
-        self.audionet_upconvlayer5 = create_upconv(ngf * 2, output_nc, True) #outermost layer use a sigmoid to bound the mask
+        self.audionet_upconvlayer2 = create_upconv(ngf * 8, ngf *4)
+        self.audionet_upconvlayer3 = create_upconv(ngf * 4, ngf * 2)
+        self.audionet_upconvlayer4 = create_upconv(ngf * 2, ngf)
+        self.audionet_upconvlayer5 = create_upconv(ngf, output_nc, True) #outermost layer use a sigmoid to bound the mask
 
 
     def forward(self, x, visual_global, visual_cropped):
@@ -119,9 +124,9 @@ class AudioNet(nn.Module):
         audioVisual_feature = torch.cat((visual_feat, audio_conv5feature), dim=1)
         
         audio_upconv1feature = self.audionet_upconvlayer1(audioVisual_feature)
-        audio_upconv2feature = self.audionet_upconvlayer2(torch.cat((audio_upconv1feature, audio_conv4feature), dim=1))
-        audio_upconv3feature = self.audionet_upconvlayer3(torch.cat((audio_upconv2feature, audio_conv3feature), dim=1))
-        audio_upconv4feature = self.audionet_upconvlayer4(torch.cat((audio_upconv3feature, audio_conv2feature), dim=1))
-        mask_prediction = self.audionet_upconvlayer5(torch.cat((audio_upconv4feature, audio_conv1feature), dim=1)) * 2 - 1
+        audio_upconv2feature = self.audionet_upconvlayer2(audio_upconv1feature)
+        audio_upconv3feature = self.audionet_upconvlayer3(audio_upconv2feature)
+        audio_upconv4feature = self.audionet_upconvlayer4(audio_upconv3feature)
+        mask_prediction = self.audionet_upconvlayer5(audio_upconv4feature) * 2 - 1
 
         return mask_prediction
